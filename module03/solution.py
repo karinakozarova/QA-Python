7949f9e5fa10b8303db0b5592b0c3adf43baba97
Karina Kozarova <karinakozarova1303@gmail.com>
2017-09-18 19:39:39 +0300

    initial commit

---
+++ module03/solution.py
@@ -0,0 +1,43 @@
+def sum_of_digits(num):
+    sum = 0
+    while num > 0:
+        digit = num % 10
+        sum += digit
+        num /= 10
+    return sum
+
+def to_digits(n):
+    pass
+
+
+def to_number(digits):
+    pass
+
+
+def count_vowels(str):
+    pass
+
+
+def count_consonants(str):
+    pass
+
+def prime_number(n):
+    pass
+
+
+def fact_digits(n):
+    pass
+
+def fibonacci(n):
+    pass
+
+
+def fib_number(n):
+    pass
+
+def palindrome(n):
+    pass
+
+def char_histogram(string):
+    pass
+
